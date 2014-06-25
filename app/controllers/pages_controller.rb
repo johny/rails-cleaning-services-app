87639ef7712c7911d8f63326a2aa53c42bd32c3d
Jan Kraus <kraus.jan@gmail.com>
2014-06-25 15:10:53 +0200

    Adds homepage landing page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def index
+
+  end
+
+  def offer
+
+  end
+
+  def contact
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # resources :orders, :only => [:new, :create]
+  resources :orders, :only => [:new, :create]
   #
-  # get '/kontakt' => "pages#contact"
-  # get '/oferta' => "pages#offer"
-  #
-  # root :to => 'pages#index'
+  get '/kontakt' => "pages#contact"
+  get '/oferta' => "pages#offer"
+  root :to => 'pages#index'
+
   #
   # namespace :admin, :path => 'admin' do
   #   get '/' => 'admin#index'
